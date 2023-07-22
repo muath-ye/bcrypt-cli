e144c8916691f55a033fe5debb67e66f707731c4
muathye <muath.ye@gmail.com>
2023-07-22 14:28:57 +0300

    init commit

---
+++ src/app.js
@@ -0,0 +1,28 @@
+const bcrypt = require("bcrypt");
+const saltRounds = 10; // the number of salt rounds to use
+
+// get the user password from the command line arguments
+const password = process.argv[2];
+
+// display usage instructions if no password is provided
+if (!password) {
+  console.log('Usage: node app.js <password>');
+  console.log('  <password>   The password to hash');
+  process.exit(1);
+}
+
+// display help message if requested
+if (password === '-h' || password === '--help') {
+  console.log('Usage: node app.js <password>');
+  console.log('  <password>   The password to hash');
+  console.log('  -h, --help   Display this help message');
+  process.exit(0);
+}
+
+// generate a salt and hash the password
+bcrypt.genSalt(saltRounds, (err, salt) => {
+  bcrypt.hash(password, salt, (err, hash) => {
+    console.log('Hashed password:');
+    console.log(hash);
+  });
+});
